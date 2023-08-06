cafcced55c2a819499ee5059c373bbb7d857f4e5
unknown <yessinekarmous25@gmail.com>
2023-08-07 00:16:34 +0100

    First React With JSX

---
+++ Week 1/Day 5/Practice/First React With JSX/first-jsx-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
