98a9c4f43ca15b80e03bb9a5db14fb8092e1c34a
unknown <yessinekarmous25@gmail.com>
2023-08-20 01:59:16 +0100

    using Mongo DB

--- Week 3/Day 2/Core/Faker Api/server.js
+++ Week 3/Day 2/Core/Faker Api/server.js
@@ -1,65 +1,99 @@
-const express=require("express")
-console.log(express)
-const Port=5000
-const app=express()
+// const express = require("express");
+// console.log(express);
+// const Port = 5000;
+// const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
+// const { faker } = require("@faker-js/faker");
 
-const { faker } = require('@faker-js/faker');
+// const CreateUser = () => {
+//   const newFake = {
+//     firstName: faker.person.firstName(),
+//     lastName: faker.person.lastName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password(),
+//     // phoneNumber:faker.phone.phoneNumber(),
+//     _id: faker.datatype.uuid(),
+//   };
+//   return newFake;
+// };
+// const FakeUser = CreateUser();
+// // console.log(FakeUser);
 
-const CreateUser=()=>{
-    const newFake = {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email:faker.internet.email(),
-        password:faker.internet.password(),
-        // phoneNumber:faker.phone.phoneNumber(),
-        _id:faker.datatype.uuid()
+// const CreateCompany = () => {
+//   const newFakeCompany = {
+//     _id: faker.datatype.uuid(),
+//     lastName: faker.person.lastName(),
+//     address: {
+//       street: faker.address.streetAddress(),
+//       city: faker.address.city(),
+//       state: faker.address.state(),
+//       zipCode: faker.address.zipCode(),
+//       country: faker.address.country(),
+//     },
+//   };
+//   return newFakeCompany;
+// };
+// const newFakeCompany = CreateCompany();
+// // console.log(newFakeCompany);
 
+// app.get("/api/users/new", (req, res) => {
+//   res.json(FakeUser);
+// });
 
-    }
-    return newFake;
-}
-const FakeUser=CreateUser();
-console.log(FakeUser)
+// app.get("/api/companies/new", (req, res) => {
+//   res.json(newFakeCompany);
+// });
 
+// app.get("/api/user/company", (req, res) => {
+//   res.json({ FakeUser, newFakeCompany });
+// });
 
-const CreateCompany=()=>{
-    const newFakeCompany={
-        _id:faker.datatype.uuid(),
-        lastName: faker.person.lastName(),
-        address:{
-            street:faker.address.streetAddress(),
-            city:faker.address.city(),
-            state:faker.address.state(),
-            zipCode:faker.address.zipCode(),
-            country:faker.address.country(),
-                    }
+// const server = app.listen(Port, () => {
+//   console.log(`Server is locked and loaded on the port: ${Port}`);
+// });
+// ///////////////////
 
-    }
-    return newFakeCompany;
-}
-const newFakeCompany=CreateCompany()
-console.log(newFakeCompany)
+const express = require("express");
+console.log(express);
+const Port = 5000;
+const app = express();
 
-app.get("/api/users/new" ,(req,res)=>{
-    res.json(FakeUser)
-})
-app.get("/api/companies/new" ,(req,res)=>{
-    res.json(newFakeCompany)
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/user/company", (req,res)=>{
-    res.json({FakeUser,newFakeCompany})
-    
-})
+const { faker } = require("@faker-js/faker");
 
+const CreateUser = () => {
+  const newFake = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    // phoneNumber:faker.phone.phoneNumber(),
+    _id: faker.datatype.uuid(),
+  };
+  return newFake;
+};
+const FakeUser = CreateUser();
+console.log("me", FakeUser);
 
+app.get("/api/users/new", (req, res) => {
+  res.json(FakeUser);
+});
+// posting new one
 
+app.post("/api/newUser", (req, res) => {
+  console.log("haniii enaaa reqqq", req.body);
+  FakeUser.street = req.body.street;
 
+  res.json({ status: "welcome new" });
+});
 
-const server=app.listen(Port, ()=>{
-    console.log(`Server is locked and loaded on the port: ${Port}`)})
-
+console.log("New me", FakeUser);
+// server
+const server = app.listen(Port, () => {
+  console.log(`Server is locked and loaded on the port: ${Port}`);
+});
